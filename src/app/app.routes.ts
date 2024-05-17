a1ad70073d49cbef91aab006fcf93b724f59026a
Melvin <melly-byte@github.com>
2024-05-17 17:44:38 -0400

    Added component folder and boiler-plate contact page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'contact', component: ContactComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
