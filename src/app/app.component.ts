ecf5be3ff5fc5183f662922da7bdcd588c58b60b
Melvin <melly-byte@github.com>
2024-05-28 05:29:54 -0400

    added basic projects page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactComponent, AboutComponent],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, ContactComponent, AboutComponent, ProjectsComponent],
+  templateUrl: './app.component.html', 
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { NgModule } from '@angular/core';
 import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'home', component: HomeComponent},
     { path: 'contact', component: ContactComponent }, 
+    { path: 'projects', component: ProjectsComponent},
     { path: 'resume', component: AboutComponent },
 ];
 
