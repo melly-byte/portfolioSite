4af8a75346e1d662590967d19adc092926a7b1e1
Melvin <melly-byte@github.com>
2024-06-17 16:37:25 -0400

    Added placeholder screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactComponent, AboutComponent, ProjectsComponent],
+  imports: [RouterOutlet, RouterLink, ContactComponent, AboutComponent, ProjectsComponent],
   templateUrl: './app.component.html', 
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  showPlaceholder = true;
   title = 'portSite';
+
+  hidePlaceholder() {
+    this.showPlaceholder = false;
+  }
 }
