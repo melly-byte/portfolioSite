6fb5abe8368c7ad7bfee46d079d1ab91517c752e
Melvin <melly-byte@github.com>
2024-06-19 19:39:53 -0400

    Test commit for any cors issues with amplify hosting

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,5 +1,9 @@
 import { Component, NgModule } from '@angular/core';
+import { secret } from '@aws-amplify/backend';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { getUrl } from '@aws-amplify/storage';
+import { OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +12,20 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
+
+
 export class AboutComponent {
+  resumeLinkString: SafeResourceUrl | undefined;
+  resumeLinkUrl: URL | undefined;
+
+  constructor(private sanitizer: DomSanitizer){}
+
+  async ngOnInit() {
+    const linkToResume = await getUrl({
+      path: "MC_R_NP.pdf"
+    })
+    console.log(linkToResume.url);
+    this.resumeLinkString = this.sanitizer.bypassSecurityTrustUrl(linkToResume.url.toString());
+    this.resumeLinkUrl = linkToResume.url;
+  }
 }
