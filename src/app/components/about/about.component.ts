a1c740856df432b0aefc065d4a2885f1796e7741
Melvin <melly-byte@github.com>
2024-06-20 17:21:27 -0400

    removed ng2-pdf-viewer to address bundle size issues and removed assets.

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,31 +1,15 @@
-import { Component, NgModule } from '@angular/core';
-import { secret } from '@aws-amplify/backend';
-import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { Component} from '@angular/core';
 import { getUrl } from '@aws-amplify/storage';
-import { OnInit } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [PdfViewerModule],
+  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 
 
 export class AboutComponent {
-  resumeLinkString: SafeResourceUrl | undefined;
-  resumeLinkUrl: URL | undefined;
 
-  constructor(private sanitizer: DomSanitizer){}
-
-  async ngOnInit() {
-    const linkToResume = await getUrl({
-      path: "MC_R_NP.pdf"
-    })
-    console.log(linkToResume.url);
-    this.resumeLinkString = this.sanitizer.bypassSecurityTrustUrl(linkToResume.url.toString());
-    this.resumeLinkUrl = linkToResume.url;
-  }
 }
