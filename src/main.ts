8137b9a0826fc7755edf2583d99a506f71a3187f
Melvin <melly-byte@github.com>
2024-06-21 16:27:46 -0400

    minor formatting in main

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { Amplify } from 'aws-amplify';
-
-//Dev env only
 import outputs from '../amplify_outputs.json';
+
 Amplify.configure(outputs);
 
 bootstrapApplication(AppComponent, appConfig)
