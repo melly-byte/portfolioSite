9360ca7cfe29e1b3d0beb7dfae00555cd3c49a02
Melvin <melly-byte@github.com>
2024-06-22 19:29:47 -0400

    Minor refactoring for lambda test

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { post } from '@aws-amplify/api';
+import { sayHello } from '../../../../amplify/functions/functionTest/resource';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  /*
+  async onClick() {
+    try {
+      const response = sayHello;
+      console.log('Response: ', response);
+    } catch (error) {
+      console.error('Error calling function: ', error);
+    }
+  }*/
 }
