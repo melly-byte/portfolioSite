5e45617f8fd85060fd0e590c43e7fe39514abea0
Melvin <melly-byte@github.com>
2024-06-26 01:24:58 -0400

    adding script for env variables, set using 'npm run config'. Added sql.js for future work

---
+++ set-env.ts
@@ -0,0 +1,29 @@
+const setEnv = () => {
+    const fs = require('fs');
+    const writeFile = fs.writeFileSync;
+
+    const targetPath = './environment.ts';
+
+    //Written during build
+    require('dotenv').config({
+        path: './.env'
+    });
+
+    const envConfigFile = `export const environment = {
+        BUNGIE_API_KEY: '${process.env.BUNGIE_API_KEY}',
+        DEST_MEMBER_ID: '${process.env.DEST_MEMBER_ID}',
+        HUNTER_CHAR_ID: '${process.env.HUNTER_CHAR_ID}'
+    };`;
+
+    console.log('The file `environment.ts` will be written with the following content: \n');
+    writeFile(targetPath, envConfigFile, (err) => {
+        if(err) {
+            console.error(err);
+            throw err;
+        } else {
+            console.log('File written successfully');
+        }
+    });
+}
+
+setEnv();
