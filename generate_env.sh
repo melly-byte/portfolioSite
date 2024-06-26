7afebd3ba06b99b0f6fe8ef359dd971943cadc5f
Melvin <melly-byte@github.com>
2024-06-26 15:46:23 -0400

    perm change

--- generate_env.sh
+++ generate_env.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#changeforpush
 echo "export const environment = { BUNGIE_API_KEY: \"$BUNGIE_API_KEY\",DEST_MEMBER_ID: \"$DEST_MEMBER_ID\", HUNTER_CHAR_ID: \"$HUNTER_CHAR_ID\", }" > environment.ts
