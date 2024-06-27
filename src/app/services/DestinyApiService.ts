fc2ef3e74c5a396ce23f75beace28e029f543829
Melvin <melly-byte@github.com>
2024-06-27 10:15:04 -0400

    Fix for env error during build

--- src/app/services/DestinyApiService.ts
+++ src/app/services/DestinyApiService.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { environment } from "../../environments/environment";
 
 //api root: https://www.bungie.net/Platform
 
