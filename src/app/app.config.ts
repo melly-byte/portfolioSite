c23b13328341c078d8c347c5e4911260b197cea6
Melvin <melly-byte@github.com>
2024-06-28 10:10:07 -0400

    Missed a file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,11 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { DestinyApiService } from './services/DestinyApiService';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
-    { provide: DestinyApiService }
   ]
 };
